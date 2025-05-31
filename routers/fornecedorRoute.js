65e136f197701ff2d588834390c63f2119de264e
felipec0elho <felipe.soares.novenove@gmail.com>
2025-05-31 12:57:18 -0300

    Correção de erro na importação da  model de fornecedor

--- routers/fornecedorRoute.js
+++ routers/fornecedorRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Fornecedor = require('../models/Fornecedor');
+const Fornecedor = require('../models/fornecedorModel');
 
 router.get('/', async (req, res) => {
   try {
